64e67c4d598de4d96e2e58a47bc0223b805fff00
Logan Barsell <loganjbars@gmail.com>
2022-07-19 00:22:01 -0700

    bio img css

--- src/components/Aboutme/Headshot.js
+++ src/components/Aboutme/Headshot.js
@@ -11,11 +11,14 @@ const Headshot = () => {
       className="box"
       sx={{
         padding: '20px',
-        margin: '10px'
+        margin: '10px',
+        position: 'relative',
+        maxWidth: '100%',
+        height: 'auto'
       }}>
 
       <img src={headshot} style={{ width: '100%', maxWidth: '350px', border: '1px solid #34a92c', opacity: 0.8 }} />
-      <div className="overlay" >
+      <div className="overlay" style={{ height: '100%', width: '100%', position: 'absolute', top: '0px', left: '0px', zIndex: '100', backgroundColor: '#34a92c', opacity: 0.1 }}>
       </div>
     </Box>
   );
