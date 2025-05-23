df37121337a831ca3c5232d1bfc2ffb9073e2117
Logan Barsell <loganjbars@gmail.com>
2025-05-23 16:17:25 -0700

    Updated portfolio + headshot

--- src/components/Aboutme/Headshot.js
+++ src/components/Aboutme/Headshot.js
@@ -1,25 +1,43 @@
 import './Headshot.css';
-import headshot from '../../images/headshot.JPG';
+import headshot from '../../images/HEADSHOT_LOGAN.jpeg';
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
 
 const Headshot = () => {
   return (
-
     <Box
-      className="box"
+      className='box'
       sx={{
         padding: '20px',
         margin: '10px',
         position: 'relative',
         maxWidth: '100%',
-        height: 'auto'
-      }}>
-
-      <img src={headshot} style={{ width: '100%', maxWidth: '270px', border: '1px solid #34a92c', opacity: 0.8 }} />
-      <div className="overlay" style={{ height: '100%', width: '100%', position: 'absolute', top: '0px', left: '0px', zIndex: '100', backgroundColor: '#34a92c', opacity: 0.1 }}>
-      </div>
+        height: 'auto',
+      }}
+    >
+      <img
+        src={headshot}
+        style={{
+          width: '100%',
+          maxWidth: '270px',
+          border: '1px solid #34a92c',
+          opacity: 0.8,
+        }}
+      />
+      <div
+        className='overlay'
+        style={{
+          height: '100%',
+          width: '100%',
+          position: 'absolute',
+          top: '0px',
+          left: '0px',
+          zIndex: '100',
+          backgroundColor: '#34a92c',
+          opacity: 0.1,
+        }}
+      ></div>
     </Box>
   );
 };
--- src/components/Portfolio/projects.js
+++ src/components/Portfolio/projects.js
@@ -1,16 +1,11 @@
-import indiluv from '../../images/portfolio/indiluv.png';
 import isurveyu from '../../images/portfolio/iSurveyU.png';
-import spotifyapp from '../../images/portfolio/spotifyapp-tn.png';
 import jbook from '../../images/portfolio/jsnote-tn.png';
 import yesdevil from '../../images/portfolio/ydpictn2.png';
-import marketingui from '../../images/portfolio/marketingui-tn.png';
 import fhs from '../../images/portfolio/fhssitetn.png';
 import bcsite from '../../images/portfolio/bcpictn.png';
 import tc6 from '../../images/portfolio/colorsixtn.png';
 import logan1 from '../../images/portfolio/portfoliov1.png';
 import portfolio2 from '../../images/portfolio/portfolio2.png';
-import audioplayer from '../../images/portfolio/audioplayer.png';
-
 
 export const projects = [
   {
@@ -24,11 +19,11 @@ export const projects = [
       'Node.js, Express, MongoDB',
       'Stripe API',
       'Microsoft OAuth Authentication',
-      'GitHub Actions'
+      'GitHub Actions',
     ],
     hosted: 'DigitalOcean',
     github: 'https://github.com/logan-barsell/yesdevil-react',
-    link: 'https://yesdevil.com/'
+    link: 'https://yesdevil.com/',
   },
   {
     title: 'iSurveyU',
@@ -39,24 +34,10 @@ export const projects = [
       'React/Redux, MaterializeCSS',
       'Node.js, Express, MongoDB',
       'Google API, Stripe API',
-      'Google OAuth Authentication'
+      'Google OAuth Authentication',
     ],
     hosted: 'Heroku',
     github: 'https://github.com/logan-barsell/emaily-app',
-  },
-  {
-    title: 'Spotify Profile',
-    type: 'Spotify Connected App',
-    img: spotifyapp,
-    desc: 'Web app for visualizing personalized Spotify data.',
-    buildList: [
-      'React/Redux, Styled Components',
-      'Node.js, Express',
-      'Spotify API',
-      'Spotify OAuth Authentication'
-    ],
-    hosted: 'Heroku',
-    github: 'https://github.com/logan-barsell/newline-spotify-app',
   },
   {
     title: 'JBook',
@@ -67,95 +48,48 @@ export const projects = [
       'React/Redux, TypeScript',
       'Bulma CSS',
       'Node.js, Express',
-      'Compiling/Bundling'
+      'Compiling/Bundling',
     ],
     hosted: 'NPM',
     github: 'https://github.com/logan-barsell/jbook',
-    link: 'https://github.com/logan-barsell/jbook'
-  },
-  {
-    title: 'indiLuv',
-    type: 'eCommerce Website',
-    img: indiluv,
-    desc: 'Online Retail Store with dynamic shopping cart functionality and secure payments with Stripe API',
-    buildList: [
-      'React/Redux',
-      'Styled Components, Material UI',
-      'Node.js, Express, MongoDB',
-      'Stripe API',
-      'Email/Pass Authentication',
-      'Firestore'
-    ],
-    hosted: 'Firebase',
-    github: 'https://github.com/logan-barsell/MERN-eCommerce',
-    link: 'https://indiluv-95c92.web.app/'
+    link: 'https://github.com/logan-barsell/jbook',
   },
   {
     title: 'Logan Barsell (V2)',
     type: 'Personal Website',
     img: portfolio2,
     desc: 'Portfolio website for a Web Developer to showcase skills and projects.',
-    buildList: [
-      'React, Material UI',
-      'Node.js, Express',
-      'GitHub Actions'
-    ],
+    buildList: ['React, Material UI', 'Node.js, Express', 'GitHub Actions'],
     hosted: 'DigitalOcean',
     github: 'https://github.com/logan-barsell/loganb-portfolio',
-    link: 'https://loganbarsell.com/'
-  },
-  {
-    title: 'Marketing.UI',
-    type: 'Microfrontend',
-    img: marketingui,
-    desc: 'Mock business website implementing a microfrontend architecture',
-    buildList: [
-      'React, Material-UI',
-      'Vue, PrimeVue, PrimeFlex',
-      'Webpack bundling',
-      'GitHub Actions'
-    ],
-    hosted: 'AWS Cloudfront',
-    github: 'https://github.com/logan-barsell/mfp-react',
-    link: 'https://d3jl9zfd971fgt.cloudfront.net/'
+    link: 'https://loganbarsell.com/',
   },
   {
     title: 'Fawkes Home Solutions',
     type: 'Business Website',
     img: fhs,
     desc: 'Website for a home-buying business in the Bay Area',
-    buildList: [
-      'HTML, CSS, jQuery',
-      'Bootstrap, SCSS',
-      'Gulp.js'
-    ],
+    buildList: ['HTML, CSS, jQuery', 'Bootstrap, SCSS', 'Gulp.js'],
     hosted: 'DigitalOcean',
     github: 'https://github.com/logan-barsell/FawkesHomeSolutions',
-    link: 'https://www.fawkeshomesolutions.com/'
+    link: 'https://www.fawkeshomesolutions.com/',
   },
   {
     title: 'Barsell Construction',
     type: 'Business Website',
     img: bcsite,
     desc: 'Website for a construction business in the Bay Area',
-    buildList: [
-      'HTML, CSS, jQuery',
-      'Materialize CSS'
-    ],
+    buildList: ['HTML, CSS, jQuery', 'Materialize CSS'],
     hosted: 'DigitalOcean',
     github: 'https://github.com/logan-barsell/barsell-construction',
-    link: 'https://barsellconstruction.com/'
+    link: 'https://barsellconstruction.com/',
   },
   {
     title: 'The Color Six',
     type: 'Band Website',
     img: tc6,
     desc: 'Website for a band to showcase music, events, photos, and  videos.',
-    buildList: [
-      'HTML, CSS, jQuery',
-      'Materialize CSS',
-      'Node.js, Express'
-    ],
+    buildList: ['HTML, CSS, jQuery', 'Materialize CSS', 'Node.js, Express'],
     hosted: 'Heroku',
     github: 'https://github.com/logan-barsell/barsell-construction',
     // link: 'https://the-color-six.herokuapp.com/'
@@ -165,29 +99,11 @@ export const projects = [
     type: 'Personal Website',
     img: logan1,
     desc: 'Portfolio website for a Web Developer to showcase skills and projects.',
-    buildList: [
-      'HTML, CSS, jQuery',
-      'Bootstrap',
-      'Node.js, Express'
-    ],
+    buildList: ['HTML, CSS, jQuery', 'Bootstrap', 'Node.js, Express'],
     hosted: 'Heroku',
     github: 'https://github.com/logan-barsell/portfolio-site',
     // link: 'https://loganbarsell-v1.herokuapp.com/'
   },
-  {
-    title: 'Custom Audio Player',
-    type: 'React Component',
-    img: audioplayer,
-    desc: 'Dynamic music player with a song list and album art display.',
-    buildList: [
-      'React',
-      'Bootstrap, CSS',
-      'React H5 Audio Player'
-    ],
-    hosted: 'Netlify',
-    github: 'https://github.com/logan-barsell/audio-player',
-    link: 'https://audioplayer-react.netlify.app/'
-  }
 ];
 
 export default { projects };
